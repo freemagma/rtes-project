c80d429f1f629d2a4393a7c2c5d30443b4e6eaf1
Charles Gunn <me@cjgunn.com>
2021-10-09 01:28:57 -0400

    init crosshatch

---
+++ crosshatch/flake.nix
@@ -0,0 +1,9 @@
+{
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      with nixpkgs.legacyPackages.${system}; {
+        devShell = mkShell { buildInputs = [ docker-compose ]; };
+      });
+}
