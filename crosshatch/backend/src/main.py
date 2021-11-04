267b3b81b675b583482eead1fe42fe4e99cb9715
Zach Minot <zjminot@gmail.com>
2021-11-04 02:13:10 -0400

    Reorganize some code into objects and make less spaghetti

--- crosshatch/backend/src/main.py
+++ crosshatch/backend/src/main.py
@@ -7,9 +7,6 @@ from .models.crossword import Crossword
 import socketio
 import uuid
 from urllib.parse import parse_qs
-import puz
-import itertools as it
-from collections import defaultdict
 
 app = FastAPI()
 
@@ -27,110 +24,46 @@ app.add_middleware(
 sio = socketio.AsyncServer(async_mode="asgi", cors_allowed_origins=[])
 sio_app = socketio.ASGIApp(sio)
 
+# temporary, eventually load from database
+crosswords = [
+    "wsj211009",
+    "wsj211011",
+    "wsj211012",
+]
+crosswords_metadata = [ { "id": crossword, "name": Crossword.get_title_from_filename(crossword) } for crossword in crosswords ]
 
-# temporary
-puzzles = {
-    "abcd": Crossword("abcd", "WSJ 10/09/2021", "wsj211009"),
-    "efgh": Crossword("efgh", "WSJ 10/11/2021", "wsj211011"),
-    "zxyz": Crossword("zxyz", "WSJ 10/12/2021", "wsj211012"),
-}
-
-
-@sio.event
-async def connect(sid, environ, auth):
-    query_string = environ["QUERY_STRING"]
-    room = parse_qs(query_string)["room"][0]
-    sio.enter_room(sid, room)
-    await sio.emit("crosswordInit", room_data[room], room=sid)
-
-
-@sio.event
-async def disconnect(sid):
-    pass
-
+room_data = {}
 
 # Routes
 @app.get("/")
 def home():
     return "Hello, World!"
 
-
 @app.get("/puzzles")
 def get_puzzles():
-    # TODO only return the necessary information we want to display
-    return list(puzzles.values())
+    # TODO this is bad
+    print(crosswords_metadata)
+    return crosswords_metadata
 
-
-room_data = {}
-
-
+# TODO puzzle_id is just the filename for now, we need to change this in the future when we add metadata to the filename
 @app.get("/play/create/{puzzle_id}")
 def create_room(puzzle_id):
-    room_code = str(uuid.uuid1())
-    room_path = f"/play/room/{room_code}"
-    room_data[room_path] = get_puzzle_data(puzzle_id)
+    room_code = str(uuid.uuid4())
+    room_path = f"/play/room/{room_code}" # TODO this is bad
+    room_data[room_path] = Crossword(puzzle_id, room_code)
     return room_path
 
+# Socket Events
+@sio.event
+async def connect(sid, environ, auth):
+    query_string = environ["QUERY_STRING"]
+    room = parse_qs(query_string)["room"][0]
+    sio.enter_room(sid, room)
+    await sio.emit("crosswordInit", room_data[room].get_puzzle_data(), room=sid)
 
-def get_puzzle_data(puzzle_id):
-    puzzle = puz.read(f"src/resources/{puzzles[puzzle_id].filename}.puz")
-
-    grid = [
-        [
-            "#" if puzzle.fill[col + row * puzzle.width] == "." else ""
-            for col in range(puzzle.width)
-        ]
-        for row in range(puzzle.height)
-    ]
-
-    numbering = puzzle.clue_numbering()
-    clues = [
-        {}
-        for _ in range(
-            max(clue["num"] for clue in [*numbering.down, *numbering.across]) + 1
-        )
-    ]
-    clues[0] = None
-    index_to_nums = defaultdict(lambda: {"down": None, "across": None})
-    clue_grid = [
-        [None if val == "#" else {"down": None, "across": None} for val in row]
-        for row in grid
-    ]
-
-    for clue, direction in [
-        *zip(numbering.down, it.repeat("down")),
-        *zip(numbering.across, it.repeat("across")),
-    ]:
-        num, flat_index, clue = clue["num"], clue["cell"], clue["clue"]
-        clues[num][direction] = clue
-
-        start_r, start_c = flat_index // puzzle.width, flat_index % puzzle.width
-        clues[num]["start_r"] = start_r
-        clues[num]["start_c"] = start_c
-        index_to_nums[(start_r, start_c)][direction] = num
-
-    for r, row in enumerate(clue_grid):
-        for c, clue_dict in enumerate(row):
-            if clue_dict is None:
-                continue
-            cur_r, cur_c = r + 1, c
-            while clue_dict["down"] is None:
-                cur_r -= 1
-                clue_dict["down"] = index_to_nums[(cur_r, cur_c)]["down"]
-            cur_r, cur_c = r, c + 1
-            while clue_dict["across"] is None:
-                cur_c -= 1
-                clue_dict["across"] = index_to_nums[(cur_r, cur_c)]["across"]
-
-    data = {
-        "grid": grid,
-        "width": puzzle.width,
-        "height": puzzle.height,
-        "clues": clues,
-        "clue_grid": clue_grid,
-    }
-    return data
-
+@sio.event
+async def disconnect(sid):
+    pass
 
 # Mount SocketIO
 app.mount("/", sio_app)
--- crosshatch/backend/src/models/crossword.py
+++ crosshatch/backend/src/models/crossword.py
@@ -1,8 +1,102 @@
-class Crossword:
-    def __init__(self, id, name, filename):
-        self.id = id
-        self.name = name
-        self.filename = filename
+from collections import defaultdict
+import itertools as it
+import puz
 
-    def asdict(self):
-        return {"name": self.name, "filename": self.filename}
+"""
+    A data representation of a currently open crossword.
+    This is what will be persisted to the database.
+
+    The crossword's key will be the room id it is associated with.
+    The constructor takes a blank instance of the crossword, copies the data,
+    and also the associated room_id.
+"""
+class Crossword:
+    # I have no idea if this is good but im doing it
+    @staticmethod
+    def get_title_from_filename(filename):
+        puzzle = puz.read(f"src/resources/{filename}.puz")
+        title = f"{puzzle.title}  {puzzle.copyright}"
+        print("TITLE: ", title)
+        return title
+
+    def __init__(self, filename, room_id):
+        self.filename = filename
+        self.room_id = room_id
+        self.set_init_crossword_data()
+
+    def set_init_crossword_data(self):
+        # TODO: Make file part of a database instead
+        puzzle = puz.read(f"src/resources/{self.filename}.puz")
+
+        grid = [
+                [
+                    "#" if puzzle.fill[col + row * puzzle.width] == "." else ""
+                for col in range(puzzle.width)
+            ]
+            for row in range(puzzle.height)
+        ]
+
+        numbering = puzzle.clue_numbering()
+        clues = [
+            {}
+            for _ in range(
+                max(clue["num"] for clue in [*numbering.down, *numbering.across]) + 1
+            )
+        ]
+        clues[0] = None
+        index_to_nums = defaultdict(lambda: {"down": None, "across": None})
+        clue_grid = [
+            [None if val == "#" else {"down": None, "across": None} for val in row]
+            for row in grid
+        ]
+
+        for clue, direction in [
+            *zip(numbering.down, it.repeat("down")),
+            *zip(numbering.across, it.repeat("across")),
+        ]:
+            num, flat_index, clue = clue["num"], clue["cell"], clue["clue"]
+            clues[num][direction] = clue
+
+            start_r, start_c = flat_index // puzzle.width, flat_index % puzzle.width
+            clues[num]["start_r"] = start_r
+            clues[num]["start_c"] = start_c
+            index_to_nums[(start_r, start_c)][direction] = num
+
+        for r, row in enumerate(clue_grid):
+            for c, clue_dict in enumerate(row):
+                if clue_dict is None:
+                    continue
+                cur_r, cur_c = r + 1, c
+                while clue_dict["down"] is None:
+                    cur_r -= 1
+                    clue_dict["down"] = index_to_nums[(cur_r, cur_c)]["down"]
+                cur_r, cur_c = r, c + 1
+                while clue_dict["across"] is None:
+                    cur_c -= 1
+                    clue_dict["across"] = index_to_nums[(cur_r, cur_c)]["across"]
+
+        self.title = puzzle.title
+        self.author = puzzle.author
+        self.grid = grid
+        self.width = puzzle.width
+        self.height = puzzle.height
+        self.clues = clues
+        self.clue_grid = clue_grid
+
+
+    def get_label_data(self):
+        data = {
+            "title": self.title,
+            "author": self.author
+        }
+        return data
+
+    def get_puzzle_data(self):
+        data = {
+            "grid": self.grid,
+            "width": self.width,
+            "height": self.height,
+            "clues": self.clues,
+            "clue_grid": self.clue_grid,
+        }
+        return data
